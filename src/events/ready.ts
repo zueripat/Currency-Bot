7780a3372e9084ad7182f466c080167fde997ac0
zueripat <ruedepatrick@gmail.com>
2023-04-26 15:49:11 +0200

    ⚡ Formatting & shorter .gitignore

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,15 @@
-import { GatewayDispatchEvents, GatewayReadyDispatchData } from '@discordjs/core';
+import {
+  GatewayDispatchEvents,
+  GatewayReadyDispatchData,
+} from "@discordjs/core";
 
 export default {
   event: GatewayDispatchEvents.Ready,
   once: true,
   execute({ data }: { data: GatewayReadyDispatchData }) {
     const { user } = data;
-    console.log(`Logged in with Bot User ${user.username}#${user.discriminator}`);
+    console.log(
+      `Logged in with Bot User ${user.username}#${user.discriminator}`
+    );
   },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,46 @@
-import 'dotenv/config';
-import { REST } from '@discordjs/rest';
-import { WebSocketManager } from '@discordjs/ws';
-import { registerEvents } from './handlers/events';
-import { registerCommands } from './handlers/commands';
-import { Client, GatewayIntentBits } from '@discordjs/core';
+import "dotenv/config";
+import { REST } from "@discordjs/rest";
+import { WebSocketManager } from "@discordjs/ws";
+import { registerEvents } from "./handlers/events";
+import { registerCommands } from "./handlers/commands";
+import { Client, GatewayIntentBits } from "@discordjs/core";
 
 // Import the Environment Variables
-const token = process.env['DISCORD_TOKEN'];
-const clientId = process.env['CLIENT_ID'];
-const guildId = process.env['GUILD_ID'];
+const token = process.env["DISCORD_TOKEN"];
+const clientId = process.env["CLIENT_ID"];
+const guildId = process.env["GUILD_ID"];
 
 // Check if the Environment Variables are set
 if (!token || !clientId || !guildId) {
-    throw new Error('Environment Variables are not set.');
+  throw new Error("Environment Variables are not set.");
 }
 
 // Create REST and WebSocket managers directly
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: "10" }).setToken(token);
 const { Guilds } = GatewayIntentBits;
 const ws = new WebSocketManager({
-    token,
-    intents: Guilds,
-    rest,
+  token,
+  intents: Guilds,
+  rest,
 });
 
 // Create a client to emit relevant events.
 const client = new Client({ rest, ws });
 
 (async () => {
-    // Register the events
-    await registerEvents(client);
+  // Register the events
+  await registerEvents(client);
 
-    // Register the commands
-    const commands = await registerCommands(client);
+  // Register the commands
+  const commands = await registerCommands(client);
 
-    // Register the commands to the server
-    await rest.put(`/applications/${clientId}/guilds/${guildId}/commands`, { body: [...commands.values()] });
+  // Register the commands to the server
+  await rest.put(`/applications/${clientId}/guilds/${guildId}/commands`, {
+    body: [...commands.values()],
+  });
 
-    // Start the WebSocket connection.
-    await ws.connect();
+  // Start the WebSocket connection.
+  await ws.connect();
 })();
 
 export { client, rest, ws };
